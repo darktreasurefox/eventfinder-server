808c228f2cd35a9d5308f59267100b9ca7150c55
shantidyah <shanti.dyah@gmail.com>
2018-07-13 13:35:53 +0700

    darksky obj weather

--- controllers/darksky.js
+++ controllers/darksky.js
@@ -1,6 +1,6 @@
 var DarkSky = require('forecast.io');
 var axios = require('axios')
-
+const moment = require ('moment')
 var options = {
     APIKey: '7154cd739ca2f07553df5146fc5e9bcf',
     timeout: 1000
@@ -23,7 +23,23 @@ module.exports = {
       //parameter [latitude],[longitude],[time] => custom
         axios.get(`https://api.darksky.net/forecast/7154cd739ca2f07553df5146fc5e9bcf/${req.query.latitude},${req.query.longitude}`)
         .then(function (response) {
-            res.send(response.data);
+            var dataWeather = response.data.daily.data[1]
+            var time = moment.unix(dataWeather.time).format("DD-MM-YYYY");
+            var sunrise = moment.unix(dataWeather.sunriseTime).format("DD-MM-YYYY HH:mm:ss")
+            var sunset = moment.unix(dataWeather.sunsetTime).format("DD-MM-YYYY HH:mm:ss")
+            console.log(time)
+
+            var weather = {
+                            location:response.data.timezone,
+                            date:time,
+                            summary:dataWeather.summary,
+                            temperatureHigh:dataWeather.temperatureHigh,
+                            temperatureLow:dataWeather.temperatureLow,
+                            windSpeed:dataWeather.windSpeed,
+                            sunriseTime:sunrise,
+                            sunsetTime:sunset
+                        }
+            res.send(weather);
         })
         .catch(function (error) {
             console.log(error);
