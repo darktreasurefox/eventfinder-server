79c6cc179037065a23c4cab68f28ec5b13b648a2
sumarsanaadi <sumarsana.adi@gmail.com>
2018-07-13 11:24:11 +0700

    add login and authorization

---
+++ middleware/authorization.js
@@ -0,0 +1,36 @@
+const {User} = require ('../models/user')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+module.exports = { 
+  userAuth : function (req,res,next) {
+    let token = req.headers.token
+    if (token) {
+      try {
+        let decoded = jwt.verify(token, process.env.jwt_key)
+        var query  = User.where({ fbId : decoded.id })
+        query.findOne(function (err, user) {
+          if (err) {
+            res.status(500)
+                .json({message : "internal server error"})
+          } else {
+            if (user == null) {
+              //no user found with above email , send response user not authorized
+              res.status(403)
+              .json ({message : "user not authorized"})
+            } else {
+              //user authenticated
+              next()
+            }
+          }
+        })
+      } catch(err) {
+        res.status(400)
+          .json ({message : "token invalid"})
+      }
+    } else {
+      res.status(401)
+      .json ({message : "token not found"})
+    }
+  }
+}
